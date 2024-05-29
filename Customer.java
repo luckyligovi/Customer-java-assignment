1d7ebc461601532007f5d4f3187b920986b587a1
luckyligovi <lucky.luvihi@student.moringaschool.com>
2024-05-29 11:08:38 +0300

    international customer

--- Customer.java
+++ Customer.java
@@ -1,10 +1,7 @@
+package assignment;
+
 public class Customer {
-    public static void main(String[] args) {
-        Customer LocalCustomer = new Customer(200, "Omondi", 2000L, "Kenya","400");
-        System.out.println(LocalCustomer.country);
-    }
-
-
+    // Attributes
     int accountNo;
     String name;
     long phoneNo;
@@ -12,27 +9,60 @@ public class Customer {
     String address;
     double balance;
 
-    
-
-    // public void deposit(){
-    // }
-
-    // public void withdraw(){
-    // }
-
-    // public void renew(){
-    // }
-
-    // public void checkBalance(){
-    // }
-
-    public Customer(int accountNo, String name, long phoneNo, String country, String address, double initialbalance) {
+    // Constructor
+    public Customer(int accountNo, String name, long phoneNo, String country, String address, double initialBalance) {
         this.accountNo = accountNo;
         this.name = name;
         this.phoneNo = phoneNo;
         this.country = country;
         this.address = address;
-        this.balance = initialbalance;
+        this.balance = initialBalance;
+    }
+
+    // Methods
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("You have deposited: " + amount);
+        } else {
+            System.out.println("Deposit amount must be positive.");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrew: " + amount);
+        } else if (amount > balance) {
+            System.out.println("Insufficient balance.");
+        } else {
+            System.out.println("Withdrawal amount must be positive.");
+        }
+    }
+
+    public void renew() {
+        // Implement the renew logic based on the specific requirements
+        System.out.println("You have renewed your account.");
+    }
+
+    public void checkBalance() {
+        System.out.println("Your current balance is: " + balance);
+    }
+
+    // Main method for testing
+    public static void main(String[] args) {
+        // Creating a customer object
+        Customer localCustomer = new Customer(200, "Omondi", 2000L, "Kenya", "400 Main St", 500.00);
+        Customer internationalCustomer = new Customer(400, "Chika", 7986726L, "Egypt", "400 Gaza", 7000.88);
+
+
+        // Testing the methods
+        System.out.println("Customer country: " + localCustomer.country);
+        localCustomer.checkBalance();
+        localCustomer.deposit(150.0);
+        localCustomer.checkBalance();
+        localCustomer.withdraw(100.0);
+        localCustomer.checkBalance();
+        localCustomer.renew();
     }
 }
-
